cc7d22980723f4af2fdc872bd87ad610a14b2a64
Hazel Cruz <hcruzlincoln@gmail.com>
2021-06-13 22:37:00 -0400

    Task List Added

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -3,24 +3,21 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 //import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '100vh',
+
   },
   main: {
-    marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2),
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(1),
   },
-  footer: {
-    padding: theme.spacing(3, 2),
-    marginTop: 'auto',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
-  },
+
 }));
 
 export default function ToDoList() {
@@ -28,23 +25,19 @@ export default function ToDoList() {
 
   return (
     <div className={classes.root}>
-      <CssBaseline />
-      <Container component="main" className={classes.main} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
-          To-Do List
-        </Typography>
+      <AppBar position ="static" color="inherit">
+        <Toolbar variant="dense">
+        <CssBaseline />
+        <Container component="main" className={classes.main} maxWidth="sm">
+          <Typography variant="h2" component="h1" gutterBottom>
+            To-Do List
+          </Typography>
 
-        <Typography variant="body1">Put in your most needed items to do</Typography>
-        <Typography variant="h3">
-            <ul>
-            <li>Buy Food</li>  
-            <li>  Buy Coffee</li>
-            </ul>
-    
-        </Typography>
-
+          <Typography variant="body1">Put in your most needed items to do</Typography>
+          
       </Container>
-    
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
