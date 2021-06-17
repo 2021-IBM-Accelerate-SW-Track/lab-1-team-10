ddd1729668a3ff1c0d8a2595572eb283451f433f
gracec4227 <46689803+gracec4227@users.noreply.github.com>
2021-06-16 20:06:45 -0400

    Fixed changing button feature

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,13 +1,13 @@
 import Button from "../button"
 
-export default function Header({ title, onAdd, showAddTask }) {
+export default function Header({ title, onAdd, showAdd }) {
     return (
         <header>
             <h1>{title}</h1>
             <Button
                 color='green'
-                text={showAddTask ? 'Close' : 'Add new Task'} 
-                onClick={onAdd} 
+                text={showAdd ? 'Close' : 'Add new Task'} 
+                onClick={() => onAdd()} 
             />
         </header>
 
